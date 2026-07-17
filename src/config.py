f818b3895901b0c2972e6163d8f3f467d11dd0be
saifullah5553 <saifullah555@yahoo.com>
2026-07-17 15:00:18 +0400

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,9 @@
+# Tickers configuration for AERP
+WATCHLIST = {
+    "NYSE/NASDAQ": ["AAPL", "MSFT", "NVDA", "TSLA", "AMZN"],
+    "PSX": ["LUCK.KA", "SYS.KA", "AIRLINK.KA", "ENGRO.KA"],
+    "CRYPTO": ["BTC-USD", "ETH-USD"],
+    "COMMODITIES": ["GC=F", "CL=F"], # Gold, Crude Oil
+    "FOREX": ["EURUSD=X", "GBPUSD=X"],
+    "INDICES": ["^GSPC", "^IXIC", "^KSE100"] # S&P 500, Nasdaq, KSE 100
+}
